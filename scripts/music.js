53f537f935ab95f90603bd8065b2efd73efcf602
Steven Greve <steven.bryan.greve@gmail.com>
2021-02-26 21:26:13 +0100

    Modified the music script.

--- scripts/music.js
+++ scripts/music.js
@@ -1,11 +1,17 @@
 /* Will pause and play the mp3 file.
 ------------------------------------*/
-const audioElement = document.getElementById("audio");
-document.getElementById("play").addEventListener("click", function(){
-    if (audioElement.paused) {
-        audioElement.play();
-    }
-    else{
-        audioElement.pause();
+const audioElement = document.querySelectorAll("#audio");
+const playElement = document.querySelectorAll("#play");
+
+audioElement.forEach(audioElement => {
+    for(let i = 0; i < playElement.length; i++){
+        playElement[i].addEventListener("click", function(){
+            if (audioElement.paused){
+                audioElement.play();
+            }
+            else{
+                audioElement.pause();
+            }
+        })
     }
 });
