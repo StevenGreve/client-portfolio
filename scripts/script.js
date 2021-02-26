ec9b9a8b6b85a6ce220386f030e2b5844898d777
Steven Greve <steven.bryan.greve@gmail.com>
2021-02-26 21:35:27 +0100

    Added sliphover effect to the main script.

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,11 @@ copyright.forEach(copyright => {
     copyright.innerHTML = `Pharaz Azimi &copy; ${new Date().getFullYear()}. All rights reserved.`;
     copyright.style.color = "#fff";
 });
+
+// SlipHover effect 
+$(function(){
+    $(".second-image-container").sliphover({
+        fontColor: "#fff",
+        reverse: false
+    });
+});
