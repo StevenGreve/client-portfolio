c3987e00707ae14e187a9809267f9b31c5713ec6
Steven Greve <steven.bryan.greve@gmail.com>
2021-03-16 23:56:13 +0100

    Renamed script to main.

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+// Copyright
+const copyright = document.querySelectorAll(".copyright");
+
+copyright.forEach(copyright => {
+    copyright.innerHTML = `Pharaz Azimi &copy; ${new Date().getFullYear()}. All rights reserved.`;
+    copyright.style.color = "#fff";
+});
