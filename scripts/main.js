a5fdc4a817f661a1354f310dc2eb879321f8b724
Steven Greve <steven.bryan.greve@gmail.com>
2021-05-04 00:15:14 +0200

    Accidentally deleted something. This file is still unchanged.

--- scripts/main.js
+++ scripts/main.js
@@ -4,18 +4,3 @@ const copyright = document.querySelectorAll(".copyright");
 copyright.forEach(copyright => {
     copyright.innerHTML = `Pharaz Azimi &copy; ${new Date().getFullYear()}. All rights reserved.`;
 });
-
-// Change the color of the text from the copyright class for the fourth slide at the mouseover event.
-function changeColor()
-{
-    copyright.forEach(copyright => {
-        copyright.style.color = "#000";
-    });
-}
-
-function defaultColor()
-{
-    copyright.forEach(copyright => {
-            copyright.style.color = "#fff";
-    });
-}
