94fd4a2531fdabd70889895fd4909e1a64f1133c
Steven Greve <steven.bryan.greve@gmail.com>
2021-05-24 03:42:30 +0200

    I added the remaining lightboxes for the works page.

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ copyright.forEach(copyright => {
     copyright.innerHTML = `Pharaz Azimi &copy; ${new Date().getFullYear()}. All rights reserved.`;
 });
 
-// Exhibition — Lightboxes
+// Exhibitions — Lightboxes
 
 /* I'n trying to prevent that annoying error, which prevents me from changing the default behavior of the event listener. 
    Error message: [Intervention] Unable to preventDefault inside passive event listener due to target being treated as 
@@ -116,7 +116,7 @@ $(document).ready(function()
         event.preventDefault();
 
         Fresco.show([
-            { url: '../img/ein-koestliches-versehen/img5.webp'},
+            { url: '../img/ein-koestliches-versehen/img5.webp', caption: 'Besides being a sweet dessert, in Italian <em>TIRAMISU</em> means pick me up, or cheer me up!'},
             { url: '../img/ein-koestliches-versehen/img3.webp'},
             { url: '../img/ein-koestliches-versehen/img2.webp'},
             { url: '../img/ein-koestliches-versehen/img6.webp'},
@@ -147,7 +147,7 @@ $(document).ready(function()
         Fresco.show([
             { url: '../img/sometimes-im-in-control/img1.webp', caption: 'A 55 minute performance in public space of the underground line U55 at Berlin&rsquo;s main station (Hauptbahnhof) – making sure everything is under control!'},
             { url: '../img/sometimes-im-in-control/img2.webp'},
-            { url: 'https://vimeo.com/433961681',
+            { url: 'https://vimeo.com/433544643',
                 options: {
                     thumbnails: false,
                     loop: true
@@ -167,7 +167,7 @@ $(document).ready(function()
         event.preventDefault();
 
         Fresco.show([
-            { url: '../img/ot-workshop/img1.webp', caption: ''},
+            { url: '../img/ot-workshop/img1.webp', caption: 'An orthopedic workshop was set up in order to produce and display products for solving the world&rsquo;s problems and fullfilling human needs. Some of my previous works—such as „Belvedere", „Things We Need to Repair the World", and „Die Weltkarte"—were used as inspiration for the workshop. <br> I decided to produce shoe inlays to help society walk better next to each other. I have used orthopedic techniques to manufacture medical insoles, which is why I created my own vacuum forming machine so that I would be able to form FP bond boards.'},
             { url: '../img/ot-workshop/img2.webp'},
             { url: '../img/ot-workshop/img3.webp'},
             { url: '../img/ot-workshop/img4.webp'},
@@ -181,7 +181,174 @@ $(document).ready(function()
             { url: '../img/ot-workshop/img12.webp'},
             { url: '../img/ot-workshop/img13.webp'},
             { url: '../img/ot-workshop/img14.webp'},
-            { url: '../img/ot-workshop/img15.webp',
+            { url: '../img/ot-workshop/img15.webp'},
+            { url: ' https://vimeo.com/433961681',
+                options: {
+                    thumbnails: false,
+                    loop: true
+                }
+            }
+        ]);
+    });
+});
+
+// Things We Need to Repair the World – Lightbox 
+
+$(document).ready(function()
+{
+    $('.works-4').on('click', function(event)
+    {
+        // The page will scroll up without this
+        event.preventDefault();
+
+        Fresco.show([
+            { url: '../img/repair-the-world/img1.webp', caption: 'This tool series is made of contrasting materials such as fragile ceramic, light rubber, or rough concrete. The tools have an ergonomic surface for the workers hand as if to simplify the utilization of them and a hole for proper hanging in the workshop. An ironic impression of the disability of finding a solution for human-made disasters and troubles is given since the tools are not comfortable to use.'},
+            { url: '../img/repair-the-world/img2.webp'},
+            { url: '../img/repair-the-world/img3.webp'},
+            { url: '../img/repair-the-world/img4.webp'},
+            { url: '../img/repair-the-world/img5.webp'},
+            { url: '../img/repair-the-world/img6.webp'},
+            { url: '../img/repair-the-world/img7.webp'},
+            { url: '../img/repair-the-world/img8.webp'},
+            { url: '../img/repair-the-world/img9.webp',
+                options: {
+                    thumbnails: false,
+                    loop: true
+                }
+            }
+        ]);
+    });
+});
+
+// A Chair for Architekten – Lightbox 
+
+$(document).ready(function()
+{
+    $('.works-5').on('click', function(event)
+    {
+        // The page will scroll up without this
+        event.preventDefault();
+
+        Fresco.show([
+            { url: '../img/a-chair-for-architekten/thumbnail.webp', caption: 'A chair made from 13 pieces of old bubble levels, providing accuracy in various positions. It constantly measures the ground surface of the person sitting on it.'},
+            { url: '../img/a-chair-for-architekten/img1.webp'},
+            { url: '../img/a-chair-for-architekten/img2.webp'},
+            { url: '../img/a-chair-for-architekten/img3.webp',
+                options: {
+                    thumbnails: false,
+                    loop: true
+                }
+            }
+        ]);
+    });
+});
+
+// Becoming a Non-Executive Chair – Lightbox
+
+$(document).ready(function()
+{
+    $('.works-6').on('click', function(event)
+    {
+        // The page will scroll up without this
+        event.preventDefault();
+
+        Fresco.show([
+            { url: '../img/non-executive-chair/thumbnail.webp'},
+            { url: '../img/non-executive-chair/img1.webp'},
+            { url: '../img/non-executive-chair/img2.webp'},
+            { url: '../img/non-executive-chair/img3.webp'},
+            { url: '../img/non-executive-chair/img4.webp'},
+            { url: '../img/non-executive-chair/img5.webp'},
+            { url: '../img/non-executive-chair/img6.webp'},
+            { url: '../img/non-executive-chair/img7.webp',
+                options: {
+                    thumbnails: false,
+                    loop: true
+                }
+            }
+        ]);
+    });
+});
+
+// Zack, Zack! – Lightbox 
+
+$(document).ready(function()
+{
+    $('.works-7').on('click', function(event)
+    {
+        // The page will scroll up without this
+        event.preventDefault();
+
+        Fresco.show([
+            { url: '../img/zack-zack/img1.webp'},
+            { url: '../img/zack-zack/img2.webp'},
+            { url: '../img/zack-zack/img3.webp',
+                options: {
+                    thumbnails: false,
+                    loop: true
+                }
+            }
+        ]);
+    });
+});
+
+// Too Early to Be Asleep Yet – Lightbox 
+
+$(document).ready(function()
+{
+    $('.works-8').on('click', function(event)
+    {
+        // The page will scroll up without this
+        event.preventDefault();
+
+        Fresco.show([
+            { url: '../img/too-early-to-be-asleep/img1.webp'},
+            { url: '../img/too-early-to-be-asleep/img2.webp'},
+            { url: '../img/too-early-to-be-asleep/img3.webp'},
+            { url: '../img/too-early-to-be-asleep/img4.webp',
+                options: {
+                    thumbnails: false,
+                    loop: true
+                }
+            }
+        ]);
+    });
+});
+
+// Eternal Eternal – Lightbox
+
+$(document).ready(function()
+{
+    $('.works-9').on('click', function(event)
+    {
+        // The page will scroll up without this
+        event.preventDefault();
+
+        Fresco.show([
+            { url: '../img/eternal-eternal/img1.webp'},
+            { url: '../img/eternal-eternal/img2.webp'},
+            { url: '../img/eternal-eternal/img3.webp',
+                options: {
+                    thumbnails: false,
+                    loop: true
+                }
+            }
+        ]);
+    });
+});
+
+// Hazardous – Lightbox 
+
+$(document).ready(function()
+{
+    $('.works-10').on('click', function(event)
+    {
+        // The page will scroll up without this
+        event.preventDefault();
+
+        Fresco.show([
+            { url: '../img/hazardous/img1.webp'},
+            { url: '../img/hazardous/img2.webp',
                 options: {
                     thumbnails: false,
                     loop: true
